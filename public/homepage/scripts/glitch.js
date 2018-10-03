1116576f169ae895cb91bb2f6b1b7258c52961ff
Pomax <pomax@nihongoresources.com>
2018-10-02 19:17:36 -0700

    faq page

--- public/homepage/scripts/glitch.js
+++ public/homepage/scripts/glitch.js
@@ -5,14 +5,23 @@ var $ = require("jquery");
 module.exports = {
   init: function() {
     let banner = $(".glitch-banner");
-    let cta = $(".glitch-cta.underlay");
-    if (banner && cta) {
-      var close = $(".glitch-cta .cta-close");
-      close.click(() => {
+    if (banner.length > 0) {
+
+      console.log("banner", banner);
+
+      let cta = $(".glitch-cta.underlay");
+      if (cta.length > 0) {
+        var close = $(".glitch-cta .cta-close");
+        close.click(() => {
+          banner.removeClass("hidden");
+          cta.addClass("hidden");
+        });
+        cta.removeClass("hidden");
+      }
+
+      else {
         banner.removeClass("hidden");
-        cta.addClass("hidden");
-      });
-      cta.removeClass("hidden");
+      }
     }
   }
 };
